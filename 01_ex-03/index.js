b109f69d8ca1b294e577533c80c39e31e1772555
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-01 01:47:31 +0500

    initial commit

---
+++ 01_ex-03/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "shAhaB uD DiN";
+console.log(personName);
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
---
+++ 01_ex-03/index.ts
@@ -0,0 +1,5 @@
+let personName = "shAhaB uD DiN";
+
+console.log(personName);
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());

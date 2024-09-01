9f93a4dedea91832f1787dc50943829a79e3a735
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-01 17:16:02 +0500

    first commit

---
+++ 02_ex_04/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let autherName = "Carol Burnett";
+let quote = `${autherName} once said, "When you have a dream, you've got to grab it and never let go."`;
+console.log(quote);
---
+++ 02_ex_04/index.ts
@@ -0,0 +1,3 @@
+let autherName : string = "Carol Burnett";
+let quote : string = `${autherName} once said, "When you have a dream, you've got to grab it and never let go."`;
+console.log(quote);

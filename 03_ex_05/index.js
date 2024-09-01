8332186ea593dc88690abcc6443e7aeff7d2ebfb
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-01 17:29:39 +0500

    upload question 5

---
+++ 03_ex_05/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_Person = "Albert Einstein";
+let message = `${famous_Person} once said, “In the middle of difficulty lies opportunity.”`;
+console.log(message);
---
+++ 03_ex_05/index.ts
@@ -0,0 +1,3 @@
+let famous_Person : string = "Albert Einstein";
+let message : string = `${famous_Person} once said, “In the middle of difficulty lies opportunity.”`;
+console.log(message);

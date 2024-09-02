93b0eaa9cde56b950318545d75c138bee23e08a8
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-02 20:22:57 +0800

    upload question 7

---
+++ 05_ex_07/index.ts
@@ -0,0 +1,24 @@
+// Addition
+let addition : number = 5 + 3;
+
+console.log(addition);
+
+// subtraction
+let number01 : number = 10;
+let number02 : number = 2;
+let subtraction = number01 - number02;
+
+console.log(subtraction);
+
+// Multiplication
+let num1 : number = 2;
+let num2 : number = 4;
+
+console.log(num1 * num2);
+
+// Division
+const num01 : number = 32;
+const num02 : number = 4;
+
+const division = num01 / num02;
+console.log(division);

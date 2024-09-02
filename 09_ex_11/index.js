30921be64402798f1b2f4806603d73b88128e264
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-02 21:22:11 +0800

    upload question 11

---
+++ 09_ex_11/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["Abu Bakar", "Taha", "Bilal", "Ahmed"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
---
+++ 09_ex_11/index.ts
@@ -0,0 +1,6 @@
+let names : string[] = ["Abu Bakar", "Taha", "Bilal", "Ahmed"];
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);

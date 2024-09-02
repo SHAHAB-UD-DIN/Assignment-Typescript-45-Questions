97fd2d945bdadc35de08db4a16c14d33436bf1af
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-02 22:05:08 +0800

    upload question 13

---
+++ 11_ex_13/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let myTran = ["car", "Bike", "Train", "Bus"];
+// using for loop
+for (let i = 0; i <= 3; i++)
+    console.log(`I would like to own a ${myTran[i]}.`);
---
+++ 11_ex_13/index.ts
@@ -0,0 +1,7 @@
+let myTran : string[] = ["car", "Bike", "Train", "Bus"];
+
+// using for loop
+for (let i = 0; i <= 3; i++)
+
+console.log(`I would like to own a ${myTran[i]}.`);
+

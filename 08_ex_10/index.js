264999a45546b925feb518786900d96fea86c9d7
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-02 21:07:43 +0800

    upload question 10

---
+++ 08_ex_10/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let num01 = 10;
+let num02 = 20;
+// Add num01 and num02
+console.log(num01 + num02);
---
+++ 08_ex_10/index.ts
@@ -0,0 +1,6 @@
+let num01 : number = 10;
+let num02 : number = 20;
+
+// Add num01 and num02
+
+console.log(num01 + num02);

1356c58b4d7c183d1eb9d53e3e2d7589eccddfad
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-02 22:22:32 +0800

    upload question 14

---
+++ 12_ex_14/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let guestList = ["Imran", "Fazan", "Subhan"];
+// using for loop
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Hi Mr.${guestList[i]},! "You are invited to dinner."`);
+}

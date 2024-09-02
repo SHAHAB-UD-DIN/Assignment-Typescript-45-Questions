642b74a1eaa8204ca7b81c8f62153ea04664aea1
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-02 22:01:47 +0500

    update

--- 12_ex_14/index.ts
+++ 12_ex_14/index.ts
@@ -2,6 +2,5 @@ let guestList : string[] = ["Imran", "Fazan", "Subhan"];
 
 // using for loop
 for (let i=0; i < guestList.length; i++){
-    console.log(`Hi Mr.${guestList[i]},! "You are invited to dinner."`);
-    
+    console.log(`Hi Mr.${guestList[i]},! "You are invited to dinner"`);
 }

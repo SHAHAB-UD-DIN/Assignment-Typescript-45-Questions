6493b3099a5b5cdff559afadb34f4604beb3abbc
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-09 23:09:49 +0500

    upload question 29

---
+++ 27_ex_29/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let favorite_fruits = ["apple", "banana", "cherry"];
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apple!");
+}
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like banana!");
+}
+if (favorite_fruits.includes("cherry")) {
+    console.log("You really like cherry!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like orange!");
+}
+if (favorite_fruits.includes("strawberry")) {
+    console.log("You really like strawberry!");
+}
---
+++ 27_ex_29/index.ts
@@ -0,0 +1,22 @@
+let favorite_fruits : string[] = ["apple", "banana", "cherry"];
+
+if(favorite_fruits.includes("apple")){
+    console.log("You really like apple!");
+    
+}
+
+if(favorite_fruits.includes("banana")){
+    console.log("You really like banana!");
+}
+
+if(favorite_fruits.includes("cherry")){
+    console.log("You really like cherry!");
+}
+
+if(favorite_fruits.includes("orange")){
+    console.log("You really like orange!");
+}    
+
+if(favorite_fruits.includes("strawberry")){
+    console.log("You really like strawberry!");
+}

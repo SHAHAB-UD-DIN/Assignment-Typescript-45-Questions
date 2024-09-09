cdcb384109f28ae7d350fbd23cbf857ea87ecfd9
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-09 22:41:03 +0500

    upload question 27

---
+++ 25_ex_27/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let alien_color = "yellow";
+if (alien_color === "green") {
+    console.log("Print a message that the player earned 5 points");
+}
+else if (alien_color === "yellow") {
+    console.log("Print a message that the player earned 10 points");
+}
+else if (alien_color === "red") {
+    console.log("Print a message that the player earned 15 points");
+}
---
+++ 25_ex_27/index.ts
@@ -0,0 +1,12 @@
+let alien_color : string = "yellow";
+
+if(alien_color === "green"){
+    console.log("Print a message that the player earned 5 points");
+    
+}else if(alien_color === "yellow"){
+    console.log("Print a message that the player earned 10 points");
+    
+}else if(alien_color === "red"){
+    console.log("Print a message that the player earned 15 points");
+    
+}

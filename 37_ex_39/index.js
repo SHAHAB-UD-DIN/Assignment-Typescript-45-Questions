d0179d3622a3d6437434c2c1d32ad11e69fc17fa
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-10 17:31:29 +0500

    upload question 39

---
+++ 37_ex_39/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+// calling the function
+console.log(city_country("Lahore", "pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Seoul", "South Korea"));
---
+++ 37_ex_39/index.ts
@@ -0,0 +1,10 @@
+function city_country(city: string, country: string): string {
+    return `${city}, ${country}`;
+}
+
+// calling the function
+console.log(city_country("Lahore", "pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Seoul", "South Korea"));
+
+

1eac3643305a7782f4e5ebbdbd41ef31f39961b6
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-10 16:53:20 +0500

    upload question 36

---
+++ 34_ex_36/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function make_shirt(size, message) {
+    console.log(`Shirt Size: "${size}"    Message: "${message}"`);
+}
+// calling the function
+make_shirt("large", "I love Pakistan");
+make_shirt("medium", "Hello World!");
---
+++ 34_ex_36/index.ts
@@ -0,0 +1,8 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`Shirt Size: "${size}"    Message: "${message}"`);
+}
+
+// calling the function
+make_shirt("large", "I love Pakistan");
+make_shirt("medium", "Hello World!");
+    

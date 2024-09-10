6ae485d42d9fa14e5de28654a927cc6f1888defc
SHAHAB-UD-DIN <150152275+SHAHAB-UD-DIN@users.noreply.github.com>
2024-09-10 16:29:36 +0500

    upload question 34

---
+++ 32_ex_34/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let favorite_pizza = ['BBQ Chicken', 'Pepperoni', 'Vegetable'];
+for (let pizza of favorite_pizza) {
+    console.log(`I like "${pizza}" pizza.`);
+}
+console.log(`I really love pizza!`);
---
+++ 32_ex_34/index.ts
@@ -0,0 +1,8 @@
+let favorite_pizza : string[] = ['BBQ Chicken', 'Pepperoni', 'Vegetable'];
+
+for (let pizza of favorite_pizza){
+    console.log(`I like "${pizza}" pizza.`);
+    
+}
+    console.log(`I really love pizza!`);
+    
